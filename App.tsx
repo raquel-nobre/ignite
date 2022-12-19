6c7b3258b314614c3664497a180c4a457a6665dc
Raquel <raquel.nobre@corebiz.ag>
2022-12-19 17:16:52 -0300

    add image svg and component Header

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
-import { Groups } from "@screens/Groups"; 
 
+import { Groups } from "@screens/Groups"; 
+import { ActivityIndicator, StatusBar } from "react-native";
+import { useFonts, Roboto_400Regular, Roboto_700Bold  } from '@expo-google-fonts/roboto';
+import { ThemeProvider} from 'styled-components'
+import theme from "./src/theme";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoader] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+ 
   return (
-    <Groups/>
+    <ThemeProvider theme={theme}> 
+     <StatusBar
+      barStyle="light-content"
+      
+     />
+      { fontsLoader ? <Groups/> : <Loading/>}
+    </ThemeProvider>
+    
   );
 }
 
